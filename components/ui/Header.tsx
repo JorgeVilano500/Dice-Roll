c64ac44879e734c4c9f6abd1ee94185fb99b0207
JorgeVilano500 <vilanova.alejandro50@gmail.com>
2026-03-20 20:48:01 -0700

    fixed some styling

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="shrink-0 w-full px-4 py-3 sm:max-w-3xl sm:mx-auto sm:my-2 flex items-center justify-between">
       <h1 className="text-2xl font-bold">Roll the Dice!</h1>
-      <ThemeToggle />
+      {/* <ThemeToggle /> */}
     </header>
   );
 }
